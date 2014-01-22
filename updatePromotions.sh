fc60a4ba0276f836a6bfaa8e8ba405eb6b9ad922
Dominic McGinnis <dominicmcginnis@gmail.com>
2014-01-22 15:38:45 -0800

    building some robustness into the script so that it sends back error messaging if login fails

--- updatePromotions.sh
+++ updatePromotions.sh
@@ -7,21 +7,41 @@ set PASS [lindex $argv 3]
 set FILE [lindex $argv 4]
 set PUSHDIR [lindex $argv 5]
 
-cd tmp;
+proc do_exit {msg} {
+    puts stderr $msg
+    exit 1
+}
+
 
 ### Copy down files
 if { $MODE == "pull" } {
+	cd tmp;
+
 	spawn scp $USER@$REMOTE_HOST:$FILE .
 	expect {
-		password: { send "$PASS\r"; exp_continue}
-	}
+		password: { 
+			send "$PASS\r"; 
+			exp_continue
+		}
+        "Permission denied, please try again." {
+            do_exit "incorrect password"
+        }
+        timeout {do_exit "timed out waiting for prompt"}
+    }
 } 
 
 ### Push up files
 if { $MODE == "push" } {
 	spawn scp $FILE $USER@$REMOTE_HOST:/$PUSHDIR/.
 	expect {
-		password: { send "$PASS\r"; exp_continue}
-	}
+		password: { 
+			send "$PASS\r"; 
+			exp_continue
+		}
+        "Permission denied, please try again." {
+            do_exit "incorrect password"
+        }
+        timeout {do_exit "timed out waiting for prompt"}
+    }
 } 
 
